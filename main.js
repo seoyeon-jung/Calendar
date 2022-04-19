d7689883a93641268ab7efb4e715b4913cf156db
seoyeon-jung <jungluchia1204@gmail.com>
2022-04-19 22:39:53 +0900

    JS 개선

--- main.js
+++ main.js
@@ -1,41 +1,88 @@
-const date = new Date();
+// date 객체 생성
+let date = new Date();
 
-const viewYear = date.getFullYear();
-const viewMonth = date.getMonth();
+const renderCalender = () => {
+  const viewYear = date.getFullYear();
+  const viewMonth = date.getMonth();
 
-const prevLast = new Date(viewYear, viewMonth, 0);
-const thisLast = new Date(viewYear, viewMonth+1, 0);
+  // 해당 연도/월 나타내기
+  document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`;
 
-const prevDate = prevLast.getDate();
-const prevDay = prevLast.getDay();
+  // 지난달 마지막 날짜(date), 이번 달 마지막 날짜(date)
+  const prevLast = new Date(viewYear, viewMonth, 0);
+  const thisLast = new Date(viewYear, viewMonth + 1, 0);
 
-const thisDate = thisLast.getDate();
-const thisDay = thisLast.getDay();
+  const PLDate = prevLast.getDate();
+  const PLDay = prevLast.getDay();
 
-const prevDates = [];
-const thisDates = [...Array(thisDate + 1).keys().slice(1)];
-const nextDates = [];
+  const TLDate = thisLast.getDate();
+  const TLDay = thisLast.getDay();
 
-const dates = prevDates.concat(thisDates, nextDates);
+  // 배열들
+  const prevDates = [];
+  const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+  const nextDates = [];
 
-document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`;
-
-// 지난 달의 마지막 날짜부터 1씩 줄어든 값을 unshift 메서드를 통해 앞쪽으로 채워넣기
-// 지난달 마지막 요일이 토요일(6)이면 그릴 필요가 없다
-if (prevDay !== 6) {
-    for (let i = 0; i < prevDay + 1; i++) {
-        prevDates.unshift(prevDate - i);
+  /* 지난 달 날짜 가져오기 
+  - 만약 지난 달 마지막 요일이 토요일(6)이라면 굳이 그릴 필요 없음
+  - 지난 달의 마지막 날짜부터 1씩 줄어든 값을 unshift 메서드를 통해 prevDates 배열 앞쪽에 채워준다
+  - unshift 메서드 : 새로운 요소를 배열의 맨 앞쪽에 추가하고, 새로운 길이를 반환
+  */
+  if (PLDay !== 6) {
+    for (let i = 0; i < PLDay + 1; i++) {
+      prevDates.unshift(PLDate - i);
     }
-}
+  }
 
-// 다음 달을 표현할 날짜들
-// 이번달 마지막 날짜의 요일을 기준으로 필요한 날짜들을 파악해서 1씩 증가시키며 채워넣기
-for (let i = 1; i < 7 - thisDay; i++) {
+  /* 다음 달 날짜 가져오기
+  - 이번 달 마지막 날짜의 요일을 기준으로 필요한 개수 파악
+  - 1부터 1씩 중가하여 nextDates 배열에 채워준다 (배열 끝부분에 push로 배열값 추가)
+   */
+  for (let i = 1; i < 7 - TLDay; i++) {
     nextDates.push(i);
-}
+  }
 
-dates.forEach((date, i) => {
-    dates[i] = `<div class="date">${date}</div>`;
-})
+  // concat : 새로운 배열을 반환해주는 배열의 메서드 중 하나
+  const dates = prevDates.concat(thisDates, nextDates);
+  const firstDateIndex = dates.indexOf(1);
+  const lastDateIndex = dates.lastIndexOf(TLDate);
 
-document.querySelector('.dates').innerHTML = dates.join('');
+  // dates 계산하기
+  // 이번달이면 this, 아니면 other (글자에 투명도를 줘서 구분을 해주기 위해서)
+  dates.forEach((date, i) => {
+    const condition = i >= firstDateIndex && i < lastDateIndex + 1
+                      ? 'this'
+                      : 'other';
+    dates[i] = `<div class="date"><span class=${condition}>${date}</span></div>`;
+  });
+
+  document.querySelector('.dates').innerHTML = dates.join('');
+
+  // 오늘 날짜 계산하기
+  const today = new Date();
+  if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
+    for (let date of document.querySelectorAll('.this')) {
+      if (+date.innerText === today.getDate()) {
+        date.classList.add('today');
+        break;
+      }
+    }
+  }
+};
+
+renderCalender();
+
+const prevMonth = () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalender();
+};
+
+const nextMonth = () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalender();
+};
+
+const goToday = () => {
+  date = new Date();
+  renderCalender();
+};
